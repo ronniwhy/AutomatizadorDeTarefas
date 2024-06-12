cb2785345498fde6dfe2552a09c6be07c25dd5f0
Ronni <ronnivirgula@gmail.com>
2024-06-12 18:29:41 -0300

    Atualizacao

---
+++ bingrewardspc.py
@@ -0,0 +1,27 @@
+import pyautogui as pgui
+import time
+
+
+pgui.hotkey('winleft')
+time.sleep(3)
+pgui.write("Microsoft Edge")
+time.sleep(1)
+pgui.hotkey('enter')
+time.sleep(1)
+pgui.write("https://www.bing.com/")
+pgui.hotkey('enter')
+time.sleep(2)
+variavel = 1
+
+for i in range(2): # Número de repetições
+    digitador = str(variavel)
+    pgui.write(f"{digitador}")
+    pgui.hotkey('space')
+    time.sleep(1)
+    pgui.hotkey('enter')
+    time.sleep(5)
+    pgui.hotkey('alt', 'd')
+    pgui.write("https://www.bing.com/")
+    pgui.hotkey('enter')
+    time.sleep(1)
+    variavel += 1
